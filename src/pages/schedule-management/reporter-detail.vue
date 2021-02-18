03c15126252f72fd0a201c3dd85c46d8ab4e8b1a
yzh <674881280@qq.com>
2021-02-18 14:18:56 +0800

    '报告详情导出pdf'

--- src/pages/schedule-management/reporter-detail.vue
+++ src/pages/schedule-management/reporter-detail.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div class="form-content">
-      <SvgIcon name="exportExcel" @click="handleExportExcel"/>
+      <SvgIcon name="exportExcel" @click="handleExportPdf"/>
       <Form
         :form-props="formProps"
         :form-data="formData"
@@ -18,7 +18,7 @@
       <WarningCensus :data="censusData"/>
     </div>
 
-    <div class="warning-content">
+    <div class="warning-content" ref="warningContent">
       <div class="title fontBlackAndBold borderBottom">预警详情</div>
       <div class="warning-type-content">
 
@@ -57,8 +57,11 @@
   import Form from "@/components/form/index.vue";
   import WarningCensus from "@/components/warning-census/index.vue";
   import SvgIcon from '@/components/svgIcon.vue';
+  import html2Canvas from 'html2canvas';
+  import JsPDF from 'jspdf';
   import { getExceptionList, getExceptionCensus } from '@/request/exception';
   import {exportExcl} from "@/utils/common";
+  import { Message } from "element-ui";
 
   export default Vue.extend({
     components:{
@@ -107,7 +110,7 @@
           fire:'火灾预警',
           helmet:'安全帽预警',
           motionless:'静止预警',
-          refectiveVest:'反反光衣预警',
+          refectiveVest:'反光衣预警',
           region:'区域预警',
           tumble:'跌倒预警',
         },
@@ -183,6 +186,53 @@
           return person.personName
         })
         return personText.join('、');
+      },
+      handleExportPdf: function () {
+        Message.info({
+          message:"正在生成文件......",
+          duration:0,
+          iconClass:"el-icon-loading",
+        });
+
+        const element = this.$refs.warningContent;
+        const _this = this;
+        html2Canvas(element, {scale: 2, useCORS: true}).then(function (canvas) {
+          const contentWidth = canvas.width;
+          const contentHeight = canvas.height;
+
+          //一页pdf显示html页面生成的canvas高度;
+          const pageHeight = contentWidth / 592.28 * 841.89;
+          //未生成pdf的html页面高度
+          let surplusHeight = contentHeight;
+          //页面偏移
+          let position = 0;
+          //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+          const imgWidth = 592.28;
+          const imgHeight = 592.28 / contentWidth * contentHeight;
+
+          const pageData = canvas.toDataURL('image/jpeg', 1.0);
+          const pdf = new JsPDF('l', 'pt','a4'); //不分页
+
+          //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+          //当内容未超过pdf一页显示的范围，无需分页
+          if (surplusHeight < pageHeight) {
+            pdf.addImage(pageData, 'JPEG', 20, 0, imgWidth, imgHeight);
+          } else {
+            while (surplusHeight > 0) {
+              pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+              surplusHeight -= pageHeight;
+              position -= 592.28;
+              //避免添加空白页
+              if (surplusHeight > 0) {
+                pdf.addPage();
+              }
+            }
+          }
+
+          pdf.save(`${_this.formData.buildTime || ''}预警信息.pdf`);
+          //@ts-ignore
+          Message.closeAll();
+        });
       },
     },
     mounted(): void {
